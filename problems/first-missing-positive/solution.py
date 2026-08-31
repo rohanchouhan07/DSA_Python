91e08e4c94215570a3776aa4bd03c1c76636e490
Rohan Chouhan <rohanchouhan.sage@gmail.com>
2026-08-31 12:24:50 +0530

    feat: update LeetCode 41. First Missing Positive solution (python3)

--- problems/first-missing-positive/solution.py
+++ problems/first-missing-positive/solution.py
@@ -1,13 +1,12 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        cnt=1
-        nums=set(nums)
-        if 1 not in nums:
-            return 1
+        # nums=set(nums)
+        # if 1 not in nums:
+        #     return 1
 
-        for i in range(1,len(nums)+1):
-            if i not in nums:
-                return i
-        return max(nums)+1
-        # st=set(nums)
-        # st.find()
+        # for i in range(1,len(nums)+1):
+        #     if i not in nums:
+        #         return i
+        # return max(nums)+1
+        nums.sort()
+        
