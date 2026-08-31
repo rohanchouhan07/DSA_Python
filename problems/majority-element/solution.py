4b64479bed93691a744ec294292be5cd1bf0d8ea
Rohan Chouhan <rohanchouhan.sage@gmail.com>
2026-08-31 10:57:59 +0530

    feat: update LeetCode 169. Majority Element solution (python3)

--- problems/majority-element/solution.py
+++ problems/majority-element/solution.py
@@ -1,8 +1,22 @@
 from collections import Counter
+
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        cnt=Counter(nums)
-        n=len(nums)
-        for num, freq in cnt.items():
-            if freq > n // 2:
-                return num
+        # cnt = Counter(nums)
+        # n = len(nums)
+        # for num, freq in cnt.items():
+        #     if freq > n // 2:
+        #         return num
+        # n=len(nums)
+        ele=0
+        cnt=0
+        for num in nums:
+            if cnt==0:
+                ele=num
+                cnt+=1
+            elif num == ele:
+                cnt+=1
+            else:
+                cnt-=1
+        return ele
