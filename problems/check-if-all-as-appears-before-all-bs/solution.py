8c96f0a6199a8b4a313870d9b234088793ac099f
Rohan Chouhan <rohanchouhan.sage@gmail.com>
2026-08-31 20:31:37 +0530

    feat: update LeetCode 2124. Check if All A's Appears Before All B's solution (python3)

--- problems/check-if-all-as-appears-before-all-bs/solution.py
+++ problems/check-if-all-as-appears-before-all-bs/solution.py
@@ -1,9 +1,9 @@
 class Solution:
     def checkString(self, s: str) -> bool:
-        sort=sorted(s)
-        ans="".join(sort)
-        if s==ans:
-            return True
-        else:
-            return False
+        return s.rfind('a') < s.find('b') or s.find('b')==-1
+        # sort=sorted(s)
+        # ans="".join(sort)
+        # return s==ans
+
+
         
